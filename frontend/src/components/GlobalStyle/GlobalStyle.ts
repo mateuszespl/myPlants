a5df9cff65df529c66b45fd7cb0edc866f265d11
Mateusz <mateusz.splawski96@gmail.com>
2020-10-11 15:49:18 +0200

    font update

--- frontend/src/components/GlobalStyle/GlobalStyle.ts
+++ frontend/src/components/GlobalStyle/GlobalStyle.ts
@@ -1,7 +1,10 @@
+import { mainTheme } from "themes/mainTheme";
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&family=Roboto:wght@400;500&display=swap');
 *{
 padding:0;
 margin:0;
-box-sizing:border-box;}`;
+box-sizing:border-box;
+font-family: ${mainTheme.fonts.main}}`;
--- frontend/src/themes/mainTheme/index.ts
+++ frontend/src/themes/mainTheme/index.ts
@@ -2,4 +2,8 @@ export const mainTheme = {
   colors: {
     main: "rgba(255, 255, 255, 1)",
   },
+  fonts: {
+    main: "'Roboto', sans-serif",
+    condensed: "'Roboto Condensed', sans-serif",
+  },
 };
