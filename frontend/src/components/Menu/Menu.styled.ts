7daace91795fd3830948434c5a0a2703c2d8cb78
Mateusz <mateusz.splawski96@gmail.com>
2020-11-12 23:49:21 +0100

    menu style update

--- frontend/src/components/Menu/Menu.styled.ts
+++ frontend/src/components/Menu/Menu.styled.ts
@@ -3,4 +3,27 @@ import styled from "styled-components";
 export const StyledMenu = styled.div`
   width: 30%;
   height: 100%;
+  background: ${({ theme }) => theme.colors.black};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ul {
+    height: 50%;
+    justify-content: space-around;
+
+    li {
+      a {
+        text-decoration: none;
+        color: ${({ theme }) => theme.colors.white};
+        font-size: ${({ theme }) => theme.fonts.xxl};
+        width: 100%;
+        transition: text-shadow 0.2s;
+
+        &:hover {
+          text-shadow: 0px 0px 2px ${({ theme }) => theme.colors.white};
+        }
+      }
+    }
+  }
 `;
--- frontend/src/components/Menu/Menu.tsx
+++ frontend/src/components/Menu/Menu.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import List from "components/List";
 import { StyledMenu } from "./Menu.styled";
+import { Link } from "react-router-dom";
 
 export const Menu: React.FC = ({}) => {
-  return <StyledMenu>Menu</StyledMenu>;
+  return (
+    <StyledMenu>
+      <List
+        className="menu"
+        listData={[
+          <Link to="/plants">Plants</Link>,
+          <Link to="/flowers">Flowers</Link>,
+          <Link to="/accesories">Accesories</Link>,
+          <Link to="/promo">Promo</Link>,
+        ]}
+      ></List>
+    </StyledMenu>
+  );
 };
